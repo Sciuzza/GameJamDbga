7c395ba6a736d7222f9079674c397d384af38963
dbga_35 <dbga_35@dbga.local>
2017-02-07 14:58:00 +0100

    prototipo stanze

---
+++ Assets/Scripts/GridManager.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class GridManager : MonoBehaviour {
+
+    public RoomManager listOfRooms;
+
+
+	// Use this for initialization
+	void Awake () {
+        
+        
+        	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
---
+++ Assets/Scripts/RoomManager.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RoomManager : MonoBehaviour {
+
+    public bool[] isDoorActive;
+    public bool isStartingRoom;
+    public bool isActiveRoom;
+    private SpriteRenderer sr;
+
+	// Use this for initialization
+	void Start ()
+    {
+        sr = GetComponent<SpriteRenderer>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (this.isStartingRoom || this.isActiveRoom)
+        {
+            this.sr.color = Color.blue;
+        }
+        else
+        {
+            this.sr.color = Color.gray;
+        }
+
+    }
+    void OnMouseDown()
+    {
+        this.isActiveRoom = true;
+    }
+}
